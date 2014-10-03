bfcc46796045fcb768dcb47783383b5507ad8707
leyven <nyhedgg@gmail.com>
2014-10-03 11:00:15 -0500

    actualizacion catalogo de ayudas

--- app/controllers/CatalogoController.php
+++ app/controllers/CatalogoController.php
@@ -4,10 +4,15 @@ class CatalogoController extends BaseController {
 			try{
 			$categoria = DB::table('categoria')->get();
 			$catalogo = DB::table('catalogo_ayuda')->get();
-						
+			$catalogocat1 = DB::table('catalogo_ayuda')->where('idCategoria','=',1)->get();
+			$catalogocat2 = DB::table('catalogo_ayuda')->where('idCategoria','=',2)->get();
+			$catalogocat3 = DB::table('catalogo_ayuda')->where('idCategoria','=',3)->get();
+			$catalogocat4 = DB::table('catalogo_ayuda')->where('idCategoria','=',4)->get();
+			$catalogocat5 = DB::table('catalogo_ayuda')->where('idCategoria','=',5)->get();
 			}catch(ErrorException $e){}
 			
-			return View::make('catalogo_ayuda')->with('catalogo', $catalogo)->with('categoria', $categoria);
+			return View::make('catalogo_ayuda')->with('catalogo', $catalogo)->with('categoria', $categoria)->with('catalogocat1',$catalogocat1)
+			->with('catalogocat2',$catalogocat2)->with('catalogocat3',$catalogocat3)->with('catalogocat4',$catalogocat4)->with('catalogocat5',$catalogocat5);
 		
 			
 		}
--- app/views/catalogo_ayuda.blade.php
+++ app/views/catalogo_ayuda.blade.php
@@ -7,36 +7,75 @@
 <br><br><h1><center>Catalogo de ayuda</center></h1>
 <div id="menu">
 <ul id="menu">
-	<li><a href="#" title="Texto">Red<input type="hidden" value="1" /> </a></li>
-	<li><a href="#" title="Texto">Teléfono<input type="hidden" value="2" /> </a></li>
-	<li><a href="#" title="Texto">Servidores<input type="hidden" value="3" /></a>
-	<li><a href="#" title="Texto">Aplicaciones<input type="hidden" value="4" /></a>
+@foreach ($categoria as $key ) 
+<div class= "categorias"
+data-Nombre=""
+data-Descripcion=""
+data-Solucion=""> 
+<li><a href="#" title="Texto">{{$key->Nombre}}<input type="hidden" value="1" /> </a></li>
+</div>	
+@endforeach
+	
 	<ul>
 		<li><a href="#" title="Texto">Sap</a></li>
 		<li><a href="#" title="Texto">Microsip</a></li>
 		<li><a href="#" title="Texto">Pigchamp</a></li>
 	</ul>
 	</li>
-	<li><a href="#" title="Texto">Desarrollo<input type="hidden" value="5" /></a></li>
+	
 </ul>
 </div><br><br><br><br><br><br>
 		<section id="contenido">
 		<table border=1 cellpadding=4 cellspacing=0>        
-		@foreach($catalogo as $cat)
-				{{'<th>'.'Nombre '.'</th>'.'<th>'.'Descripción'.'</th>'.'<th>'.'Solución'
-				.'</th>'.
-				'<tr>'.'<td>'.$cat->Nombre.'</td>'.'<td>'.$cat->Descripcion.'</td>'.'<td>'.$cat->Solucion.'</td>'.'</tr>'}}
-		@endforeach  
+		
+
+
+		<div id="category1">
+
+		<th>Nombre </th><th>Descripción</th><th>Solución
+				</th> 
+			@foreach($catalogocat1 as $key)
+			{{'<tr>'.'<td>'.$key->Nombre.'</td>'.'<td>'.$key->Descripcion.'</td>'.'<td>'.$key->Solucion.'</td>'.'</tr>'}}
+			@endforeach  
+		</div>
+		
+		<div id="category2">
+		
+		<th>Nombre </th><th>Descripción</th><th>Solución
+				</th> 
+			@foreach($catalogocat2 as $key)
+			{{'<tr>'.'<td>'.$key->Nombre.'</td>'.'<td>'.$key->Descripcion.'</td>'.'<td>'.$key->Solucion.'</td>'.'</tr>'}}
+			@endforeach  
+		</div>
+		
+		<div id="category3">
+		
+		<th>Nombre </th><th>Descripción</th><th>Solución
+				</th> 
+			@foreach($catalogocat3 as $key)
+			{{'<tr>'.'<td>'.$key->Nombre.'</td>'.'<td>'.$key->Descripcion.'</td>'.'<td>'.$key->Solucion.'</td>'.'</tr>'}}
+			@endforeach  
+		</div>
+		
+		<div id="category4">
+		
+		<th>Nombre </th><th>Descripción</th><th>Solución
+				</th> 
+			@foreach($catalogocat4 as $key)
+			{{'<tr>'.'<td>'.$key->Nombre.'</td>'.'<td>'.$key->Descripcion.'</td>'.'<td>'.$key->Solucion.'</td>'.'</tr>'}}
+			@endforeach  
+		</div>
+		
+		<div id="category5">
+		Desarrollo <br>
+		<th>Nombre </th><th>Descripción</th><th>Solución
+				</th> 
+			@foreach($catalogocat5 as $key)
+			{{'<tr>'.'<td>'.$key->Nombre.'</td>'.'<td>'.$key->Descripcion.'</td>'.'<td>'.$key->Solucion.'</td>'.'</tr>'}}
+			@endforeach  
+		</div>
+
+
 		</table>	
 		</section>
 		
-
-<script>
-
-$('#contenido').delegate('img','click', function(){
-	$('#imgContent2').attr('src',$(this).attr('src').replace('thumb','large'));
-	$('#titleContent').html($(this).attr('title'));
-	$('#descriptionContent').html($(this).attr('alt'));
-});
-
-</script>

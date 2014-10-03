be902f615e055f2bc4c36dcd740296df8dc825c0
GeorgeMontero <thegeorgemg@gmail.com>
2014-10-03 09:33:15 -0500

    Control Acrividades

--- app/views/actividades.blade.php
+++ app/views/actividades.blade.php
@@ -1,45 +1,35 @@
 <head>
-
-<title>Control de actividades</title>
-
 <!-- COLOCAR CSS a la TABLA -->
-<link rel="stylesheet" type="text/css" href="../public/css/estilo.css" media="screen" />
+	<meta charset="UTF-8">
+	<!--<link rel='shortcut icon' href='img/logo-ico.png' type='image/png'>-->
+	<title>Control de Actividades</title>
+	<link rel="stylesheet" type="text/css" href="css/estilo.css" media="screen" />
 </head>
 
-
-<body BGCOLOR="#47A484">
-
-
-<div class="actividades" id="actividades">
-
-<table border = "1" width = "100%"> 
-
-
-<tr> 
-<th>Folio</th> 
-<th>Nombre actividad</th> 
-<th>Id usuario</th>
-<th>Id actividad</th>
-<th>Fecha inicio</th>
-<th>Fecha termino</th>
-<th>Descripcion</th>
-
-</tr> 
-@foreach ($var as $variable)
-<tr> 
-<td> {{$variable -> folio}} </td> 
-<td> {{$variable -> nombre_Actividad}} </td> 
-<td> {{$variable -> id_Usuario}} </td> 
-<td> {{$variable -> id_Actividad}} </td> 
-<td> {{$variable -> fecha_Inicio}} </td> 
-<td> {{$variable -> fecha_Termino}} </td> 
-<td> {{$variable -> descripcion}} </td> 
-</tr> 
-
-</div>
-	@endforeach
-
-</table> 
-
-
+<body>
+	<img id="logo"src="img/buenaventura.gif" alt="">
+	<div id="container">
+		<table border = "1" width = "100%"> 
+			<tr> 
+				<th>Folio</th> 
+				<th>Nombre actividad</th> 
+				<th>Id usuario</th>
+				<th>Id actividad</th>
+				<th>Fecha inicio</th>
+				<th>Fecha termino</th>
+				<th>Descripcion</th>
+			</tr> 
+			@foreach ($var as $variable)
+			<tr> 
+				<td> {{$variable -> folio}} </td> 
+				<td> {{$variable -> nombre_Actividad}} </td> 
+				<td> {{$variable -> id_Usuario}} </td> 
+				<td> {{$variable -> id_Actividad}} </td> 
+				<td> {{$variable -> fecha_Inicio}} </td> 
+				<td> {{$variable -> fecha_Termino}} </td> 
+				<td> {{$variable -> descripcion}} </td> 
+			</tr> 
+			@endforeach
+		</table> 
+	</div>
 </body>
